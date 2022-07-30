cd6962bfcb7f4f328ff7bed786d80c018e791229
Boudjada Yasser <by.root96@gmail.com>
2022-07-30 15:55:37 +0100

    Delete run.sh

--- docker/run.sh
+++
@@ -1,4 +0,0 @@
-# Reason for "-nolisten tcp", not documented within Xvfb manpages (for who knows what reason)
-# https://superuser.com/questions/855019/make-xvfb-listen-only-on-local-ip
-Xvfb :99 -screen 0 640x480x8 -nolisten tcp &
-python3 examples.py
